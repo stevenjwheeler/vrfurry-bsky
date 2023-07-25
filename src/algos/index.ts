8c44eb32b8fbe4691d3670e0a7162b51df9e7f09
Steven Wheeler <steven@stevenwheeler.co.uk>
2023-07-26 00:24:28 +0100

    added NSFW feed

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -4,11 +4,13 @@ import {
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import * as sfwFeed from './sfw-feed'
+import * as nsfwFeed from './nsfw-feed'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
   [sfwFeed.shortname]: sfwFeed.handler,
+  [nsfwFeed.shortname]: nsfwFeed.handler,
 }
 
 export default algos
