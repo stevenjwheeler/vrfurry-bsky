ff64b74eeb057a33007e94a525605fbe9869ccd3
Steven Wheeler <steven@stevenwheeler.co.uk>
2023-07-25 22:50:21 +0100

    algo logic

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -3,12 +3,12 @@ import {
   QueryParams,
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as whatsAlf from './whats-alf'
+import * as sfwFeed from './sfw-feed'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
-  [whatsAlf.shortname]: whatsAlf.handler,
+  [sfwFeed.shortname]: sfwFeed.handler,
 }
 
 export default algos
